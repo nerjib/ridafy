6e1d97493cc6e8dc35dc06b1428604710ffd8268
nerjib <kabirnajib0@gmail.com>
2021-04-13 18:12:09 +0100

    add books

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -5,68 +5,38 @@ const moment = require ('moment')
 const router = express.Router();
 const db = require('../dbs/index');
 
-var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, './public/images');
-    },
-    filename: (req, file, cb) => {
-      console.log(file);
-      var filetype = '';
-      if(file.mimetype === 'image/gif') {
-        filetype = 'gif';
-      }
-      if(file.mimetype === 'image/png') {
-        filetype = 'png';
-      }
-      if(file.mimetype === 'image/jpeg') {
-        filetype = 'jpg';
-      }
-      cb(null, 'image-' + Date.now() + '.' + filetype);
-    }
-});
-var upload = multer({storage: storage});
-
-
-router.post('/upload',upload.single('file'),function(req, res, next) {
-    console.log(req.file);
-    if(!req.file) {
-      res.status(500);
-      return next(err);
-    }
-    res.json({ fileUrl: 'https://ridafyapp.herokuapp.com' + req.file.filename });
-  })
 
 router.get('/', async (req, res) => {
- 
-});
-
-
-router.get('/:id', async (req, res) => {
-  const text = 'SELECT * FROM users WHERE id = $1';
-  // console.log(req.params.id);
-  try {
-    const { rows } = await db.query(text, [req.params.id]);
-    if (!rows[0]) {
-      return res.status(404).send({ message: 'User not found' });
+    const getAllQ = 'SELECT * FROM books';
+    try {
+      // const { rows } = qr.query(getAllQ);
+      const { rows } = await db.query(getAllQ);
+      return res.status(201).send(rows);
+    } catch (error) {
+      if (error.routine === '_bt_check_unique') {
+        return res.status(400).send({ message: 'books not available' });
+      }
+      return res.status(400).send(`${error} jsh`);
     }
-    return res.status(200).send(rows[0]);
-  } catch (error) {
-    return res.status(400).send(error);
-  }
-});
+  });
+
 
 router.post('/', async (req, res) => {
     
     const createUser = `INSERT INTO
-    users (name,email,password,phone_no,gender,time)
+    users (title,author_id,description,sample_location,chapters_count,category_id,cover_location,reciter_id,price,created_at)
     VALUES ($1, $2,$3,$4,$5,$6) RETURNING *`;  
   const values = [
-  req.body.name,
-  req.body.email,
-  req.body.password,
-  req.body.phone_no,
-  req.body.gender,
-  moment(new Date()),
+  req.body.title,
+  req.body.author_id,
+  req.body.description,
+  req.body.sample_location,
+  req.body.chapters_count,
+  req.body.category_id,
+  req.body.cover_location,
+  req.body.reciter_id,
+  req.body.price,
+  moment(new Date())
   ];
   try {
   const { rows } = await db.query(createUser, values);

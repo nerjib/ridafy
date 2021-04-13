bcc8fa2d02bf51d23573ee0986c2852a8ea0b2cc
nerjib <kabirnajib0@gmail.com>
2021-04-13 18:43:49 +0100

    author

---
+++ src/controllers/authors.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const moment = require ('moment')
+
+const router = express.Router();
+const db = require('../dbs/index');
+
+router.get('/', async (req, res) => {
+  const getAllQ = 'SELECT * FROM uauthors';
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ);
+    return res.status(201).send(rows);
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return res.status(400).send({ message: 'User with that EMAIL already exist' });
+    }
+    return res.status(400).send(`${error} jsh`);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const text = 'SELECT * FROM authors WHERE id = $1';
+  // console.log(req.params.id);
+  try {
+    const { rows } = await db.query(text, [req.params.id]);
+    if (!rows[0]) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+    return res.status(200).send(rows[0]);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+});
+
+
+
+router.post('/', async (req, res) => {
+    
+    const createUser = `INSERT INTO
+    authors (author_name,gender,state_id,user_id,created_at)
+    VALUES ($1, $2,$3,$4,$5) RETURNING *`;  
+  const values = [
+  req.body.author_name,
+  req.body.gender,
+  req.body.state_id,
+  req.body.user_id,
+  moment(new Date())
+  ];
+  try {
+  const { rows } = await db.query(createUser, values);
+  // console.log(rows);
+  const data = {
+    status: 'success',
+    data: {
+      message: 'User added successfully​',
+      Name: rows[0].author_name,
+      
+    },
+  };
+  return res.status(201).send(data);
+  } catch (error) {
+  return res.status(400).send(error);
+  }
+  
+  });
+ 
+
+module.exports = router;

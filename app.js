7a6e8cc59c30a42c19f8657b3f7666cfb73960a4
nerjib <kabirnajib0@gmail.com>
2021-04-14 21:16:37 +0100

    verify mail

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ const storage = multer.diskStorage({
   });
 
 app.use('/api/v1/users',Auth.verifyToken, Users)
-app.use('/api/v1/auth/signin',Auth.verifyToken, AuthSignIn)
+app.use('/api/v1/auth/signin', AuthSignIn)
 app.use('/api/v1/auth/signup', AuthUsers)
 app.use('/api/v1/books', Auth.verifyToken,Books)
 app.use('/api/v1/authors',Auth.verifyToken, Authors)

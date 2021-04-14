cf2edcc2d31735dd9b1b43d80e03cdaf3e0c4dd9
nerjib <kabirnajib0@gmail.com>
2021-04-14 09:02:59 +0100

    category add and get

---
+++ src/controllers/categories.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const moment = require ('moment')
+
+const router = express.Router();
+const db = require('../dbs/index');
+
+router.get('/', async (req, res) => {
+  const getAllQ = 'SELECT * FROM categories';
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ);
+    return res.status(201).send(rows);
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return res.status(400).send({ message: 'User with that EMAIL already exist' });
+    }
+    return res.status(400).send(`${error} jsh`);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const text = 'SELECT * FROM categories WHERE id = $1';
+  // console.log(req.params.id);
+  try {
+    const { rows } = await db.query(text, [req.params.id]);
+    if (!rows[0]) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+    return res.status(200).send(rows[0]);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+});
+
+
+
+create table categories(
+    id serial,
+    category_name varchar(100) not null,
+    user_id int not null,
+    created_at timestamp null default null,
+    updated_at timestamp null default null,
+    PRIMARY KEY (id)
+    )
+    
+router.post('/', async (req, res) => {
+    
+    const createUser = `INSERT INTO
+    categories (category_name,user_id,created_at)
+    VALUES ($1,$2,$3) RETURNING *`;  
+  const values = [
+      req.body.category_name,
+  req.body.user_id,
+  moment(new Date())
+  ];
+  try {
+  const { rows } = await db.query(createUser, values);
+  // console.log(rows);
+  const data = {
+    status: 'success',
+    data: {
+      message: 'Category added successfully​',
+      Category_Name: rows[0].category_name,
+    },
+  };
+  return res.status(201).send(data);
+  } catch (error) {
+  return res.status(400).send(error);
+  }
+  
+  });
+ 
+
+module.exports = router;

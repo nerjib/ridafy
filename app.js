e69a762087c0299619247682b014c1e2a2467719
nerjib <kabirnajib0@gmail.com>
2021-04-16 16:08:10 +0100

    test node mailer

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ const storage = multer.diskStorage({
     }
   };
   
-  const upload = multer({
+  const upload1 = multer({
     storage,
     fileFilter,
   });
@@ -99,6 +99,12 @@ app.use('/api/v1/wishlists',Auth.verifyToken, WishLists)
 app.use('/api/v1/ratings', Auth.verifyToken,Ratings)
 app.use('/api/v1/reciters',Auth.verifyToken, Reciters)
 app.use('/api/v1/reviews', Auth.verifyToken,Reviews)
+const upload = require('./multer')
+const cloudinary = require('./cloudinary')
+const fs = require('fs');
+
+
+
 
 
 
@@ -121,6 +127,36 @@ app.post('/hhh',(req,res)=>{
 app.get('/',(req,res)=>{
     res.send('welcome to ridafy')
 })
+
+
+
+
+
+app.use('/upload-images', upload.array('image'), async (req, res) => {
+
+    const uploader = async (path) => await cloudinary.uploads(path, 'Images');
+  
+    if (req.method === 'POST') {
+      const urls = []
+      const files = req.files;
+      for (const file of files) {
+        const { path } = file;
+        const newPath = await uploader(path)
+        urls.push(newPath)
+        fs.unlinkSync(path)
+      }
+  
+      res.status(200).json({
+        message: 'images uploaded successfully',
+        data: urls
+      })
+  
+    } else {
+      res.status(405).json({
+        err: `${req.method} method not allowed`
+      })
+    }
+  })
 
 
 

4bdcf50069ec05a6e81e18a5bcdc94597e143ebe
nerjib <kabirnajib0@gmail.com>
2021-05-04 22:16:13 +0100

    wit api

--- methods.js
+++ methods.js
@@ -24,7 +24,7 @@ const json = {
       //  console.log(JSON.stringify(json))
       //  console.log('kkkkk '+json.entry[0].messaging[0].message['text'])
         const message = 'json.entry[0].messaging[0].message.text'
-        const id = 407860476736240
+        const id = 2784001451616678
 return{ message, id}
     }
 }

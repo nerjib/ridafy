4d2e29be633b1a8f3d88804db1cc2b5be7ab4d15
nerjib <kabirnajib0@gmail.com>
2021-05-05 09:37:30 +0100

    wit api

--- methods.js
+++ methods.js
@@ -26,7 +26,7 @@ const json = {
         console.log('kkkkk '+JSON.stringify(json.entry[0].messaging[0].message.nlp))
         const id = json.entry[0].messaging[0].sender.id
         const message = json.entry[0].messaging[0].message.text
-        const intent = 'json.entry[0].messaging[0].message.nlp.entities.intent[0].value'
+        const intent = json.entry[0].messaging[0].message.nlp.entities.intent[0].value
 
 return{ message, id, intent}
     }

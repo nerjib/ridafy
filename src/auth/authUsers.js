11873297d145dbdeec11f68e13b3e7a577c9bb14
nerjib <kabirnajib0@gmail.com>
2021-06-06 15:20:34 +0100

    token expiration

--- src/auth/authUsers.js
+++ src/auth/authUsers.js
@@ -101,7 +101,7 @@ let testAccount = await nodemailer.createTestAccount();
                     }
                 });
                 */
-              const info = await   transporter.sendMail({
+           /*   const info = await   transporter.sendMail({
                     from: '"Ridafy " <kadruwassa@gmail.com>', // sender address
                     to: 'kabirnajib0@gmail.com, najib@kadruwassa.ng', // list of receivers
                     subject: "Hello ", // Subject line
@@ -111,9 +111,25 @@ let testAccount = await nodemailer.createTestAccount();
                         return console.log(error);
                       }
                       console.log('Email sent: ' + info.response);
-                    });
-
-                return res.send(info.response);
+                    });*/
+                    let message = {
+                      from: 'Sender Name <sender@example.com>',
+                      to: 'Recipient <kabirnajib0@gmail.com>',
+                      subject: 'Nodemailer is unicode friendly ✔',
+                      text: 'Hello to myself!',
+                      html: '<p><b>Hello</b> to myself!</p>'
+                  };
+                    transporter.sendMail(message, (err, info) => {
+                      if (err) {
+                          console.log('Error occurred. ' + err.message);
+                          return process.exit(1);
+                      }
+              
+                      console.log('Message sent: %s', info.messageId);
+                      // Preview only available when sending through an Ethereal account
+                      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+                  });
+            //    return res.send(info.response);
 
            /*     const getAllQ = 'SELECT * FROM users';
                 try {

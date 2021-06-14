68a1320d5ef217b5684452a94618c176be891250
nerjib <kabirnajib0@gmail.com>
2021-06-14 19:39:10 +0100

    gmail nodemailer'

--- src/auth/authUsers.js
+++ src/auth/authUsers.js
@@ -64,8 +64,9 @@ const mailgun = require("mailgun-js");
 
 
            router.get('/kk', async (req, res) => {
+          await   main('kabirnajib0@gmail.com')
 
-
+/*
 
 let testAccount = await nodemailer.createTestAccount();
                 var transporter = nodemailer.createTransport({
@@ -90,41 +91,37 @@ let testAccount = await nodemailer.createTestAccount();
                     else
                       console.log(info);
                  });
-
+*/
             });
 
-async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+async function main(kk) {
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
     auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  });
+           user: 'ridafyinfp@gmail.com',
+           pass: 'ridafyapp2020'
+       }
+   });
+      let message = {
+        from: 'Ridafy App <verify@ridafyapp.ng>',
+        to: `${kk} <${kk}>`,
+        subject: 'Account Verification',
+        text: 'Click the link to veryfy your account',
+        html: `<b><a href='m.me'> click here</a></b>`
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: "kabirnajib0@gmail.com, najib@kadruwassa.ng", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
+    };
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    transporter.sendMail(message, function (err, info) {
+      if(err)
+        console.log(err)
+      else
+        console.log(info);
+   });
 
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
+ }
+
+
+
 
 router.get('/maill',async(req,res)=>{
   

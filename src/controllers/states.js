dbfcac1fd08e91fd31b6b1a3131e5500c8811230
nerjib <kabirnajib0@gmail.com>
2021-07-09 09:11:49 +0100

    get data of particular states

--- src/controllers/states.js
+++ src/controllers/states.js
@@ -18,5 +18,19 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:state', async (req, res) => {
+    const getAllQ = 'SELECT * FROM state where state =$1';
+    try {
+      // const { rows } = qr.query(getAllQ);
+      const { rows } = await db.query(getAllQ,[req.params.state]);
+      return res.status(201).send(rows);
+    } catch (error) {
+      if (error.routine === '_bt_check_unique') {
+        return res.status(400).send({ message: 'User with that EMAIL already exist' });
+      }
+      return res.status(400).send(`${error} jsh`);
+    }
+  });
+
 
 module.exports = router;

e0c088fa55bc3a4b2459fec096eaa515bfa179ce
nerjib <kabirnajib0@gmail.com>
2021-07-10 15:27:03 +0100

    get distinct lga';

--- src/controllers/states.js
+++ src/controllers/states.js
@@ -32,5 +32,19 @@ router.get('/:state', async (req, res) => {
     }
   });
 
+  router.get('/getlga/:state', async (req, res) => {
+    const getAllQ = 'SELECT distinct lga FROM state where state =$1';
+    try {
+      // const { rows } = qr.query(getAllQ);
+      const { rows } = await db.query(getAllQ,[req.params.state]);
+      return res.status(201).send(rows);
+    } catch (error) {
+      if (error.routine === '_bt_check_unique') {
+        return res.status(400).send({ message: 'User with that EMAIL already exist' });
+      }
+      return res.status(400).send(`${error} jsh`);
+    }
+  });
+
 
 module.exports = router;
